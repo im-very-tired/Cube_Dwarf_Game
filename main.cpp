9f9c0548cbea7990f4fc0268d99acd6bf7eab34a
Atticus <atticus.butchart@gmail.com>
2023-10-28 15:38:12 -0700

    add glew library next

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include <glfw/glfw3.h>
+#include <glm/glm.hpp>
 
 using namespace std;
 
 int main()
 {
     cout << "Hewwo Wowld!" << endl;
+    system("PAUSE");
 
     return 0;
 }
