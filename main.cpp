4f2e4617d0bae308cd5ff81ba1b370240ece959b
Atticus <atticus.butchart@gmail.com>
2023-10-28 21:54:24 -0700

    added comments and other stuff

--- main.cpp
+++ main.cpp
@@ -5,28 +5,30 @@ using namespace std;
 
 int main()
 {
+//vars
+
     string user_name;
     string entered_pass;
     string pass = "shrimp";
 
-    cout << "enter user name: \n";
-    cin >> user_name;
+    cout << "enter user name: \n";// might keep the space in username
+    cin >> user_name;//read entered username
     if (user_name == "test_acc_0")
     {
         cout << "enter password:\n";
         cin >> entered_pass;
-        if (entered_pass == pass)
+        if (entered_pass == pass)//password checker
         {
             cout << "Hello Dev" << user_name << ", Welcome to a purgatory of your own making\n";
 
-            system("PAUSE");
+            system("PAUSE"); //end program
             return 0;
         }
         else
         {
             cout << "incorect password \n";
 
-            system("PAUSE");
+            system("PAUSE"); //end program
             return 0;
         }
     }
@@ -34,10 +36,7 @@ int main()
     {
         cout << "sorry, this account does not exist\n";
 
-        system("PAUSE");
+        system("PAUSE"); //end program
         return 0;
     }
-
-    system("PAUSE");
-    return 0;
 }
