b7b3d20c5a639866c74f98337359c694bc131878
Atticus <atticus.butchart@gmail.com>
2023-10-28 21:41:47 -0700

    more password stuff, will work on graphics eventualy

--- main.cpp
+++ main.cpp
@@ -6,16 +6,36 @@ using namespace std;
 int main()
 {
     string user_name;
+    string entered_pass;
+    string pass = "shrimp";
 
     cout << "enter user name: \n";
     cin >> user_name;
     if (user_name == "test_acc_0")
     {
-        cout << "Hello Dev" << user_name << ", Welcome to a purgatory of your own making\n";
+        cout << "enter password:\n";
+        cin >> entered_pass;
+        if (entered_pass == pass)
+        {
+            cout << "Hello Dev" << user_name << ", Welcome to a purgatory of your own making\n";
+
+            system("PAUSE");
+            return 0;
+        }
+        else
+        {
+            cout << "incorect password \n";
+
+            system("PAUSE");
+            return 0;
+        }
     }
     else
     {
-        cout << "Hello " << user_name << ", Welcome\n";
+        cout << "sorry, this account does not exist\n";
+
+        system("PAUSE");
+        return 0;
     }
 
     system("PAUSE");
